8c1bbf7223a1263d5318498d76bd5977e4f59160
guayom <guayo.mena@gmail.com>
2016-09-08 23:25:24 -0500

    Create admin only in development environment I don't want to put my password on an environment variable @heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 #Admin
-Admin.create(email: "guayo.mena@gmail.com", password: ENV["admin_password"], password_confirmation: ENV["admin_password"] )
+
+Admin.create(email: "guayo.mena@gmail.com", password: ENV["admin_password"], password_confirmation: ENV["admin_password"] ) if Rails.env.development?
+
 #Baseball teams
 @football = Sport.new(title: 'Football')
 @nfl = League.new(sport: @football, name: "NFL", abbreviation: "NFL" )
