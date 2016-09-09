f28541c940a1786b62bfad72d100e995a9f7fda8
guayom <guayo.mena@gmail.com>
2016-09-08 23:10:03 -0500

    Created seed for football

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-sports = %w(Basketball Soccer Football Tennis)
-sports.each_with_index do |title, i|
-	puts "Creating #{i+1}/#{sports.count} sport"
-	sport = Sport.new(title: title)
-	sport.image = File.open(Rails.root.join("test/fixtures/images/#{title}.jpg"))
-	sport.color = Faker::Color.color_name
-	sport.save!
-	8.times do
-		team = sport.teams.new(name: Faker::Team.name)
-		team.logo = File.open(Dir[Rails.root.join('test/fixtures/logos/*')].sample)
-		team.save!
-	end	
-end
+#Baseball teams
+@football = Sport.new(title: 'Football')
+@nfl = League.new(sport: @football, name: "NFL", abbreviation: "NFL" )
