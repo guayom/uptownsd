816df7e8b3b39fcb16528500f01480a4805b93b5
guayom <guayo.mena@gmail.com>
2016-09-09 08:56:08 -0500

    Seeded teams scraped from internet sources

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,22 @@ end
 @leagues.each do |league|
 	League.create(sport_id: Sport.where(title: league[:sport]).first.id, name: league[:name], abbreviation: league[:abbreviation])
 end
+
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+football_teams = Nokogiri::HTML(open("https://en.wikipedia.org/wiki/Lists_of_National_Football_League_team_seasons"))
+football_teams.css(".wikitable .sortbottom .mw-redirect").each do |row|
+	Team.create(name: row.text, sport_id: Sport.where(title: "Football").first.id)
+end
+
+baseball_teams = Nokogiri::HTML(open("http://mlb.mlb.com/team/index.jsp"))
+baseball_teams.css(".team h5 a").each do |row|
+	Team.create(name: row.text, sport_id: Sport.where(title: "Baseball").first.id)
+end
+
+baseball_teams = Nokogiri::HTML(open("http://www.nba.com/teams/"))
+baseball_teams.css(".nbateamname a").each do |row|
+	Team.create(name: row.text, sport_id: Sport.where(title: "Basketball").first.id)
+end
