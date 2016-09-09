c7a4ae701a1f2f2c78e2291d8aec233aea99fa87
guayom <guayo.mena@gmail.com>
2016-09-09 00:10:04 -0500

    Seed sports and leagues

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
 #Admin
-
 Admin.create(email: "guayo.mena@gmail.com", password: ENV["admin_password"], password_confirmation: ENV["admin_password"] ) if Rails.env.development?
 
-#Baseball teams
-@football = Sport.new(title: 'Football')
-@nfl = League.new(sport: @football, name: "NFL", abbreviation: "NFL" )
+@sports = ["Football", "Baseball", "Basketball"]
+@sports.each do |sport|
+	Sport.create(title: sport)
+end
+
+@leagues = [
+	{:sport => "Football", :name => "NFL", :abbreviation => "NFL"},
+	{:sport => "Baseball", :name => "MLB", :abbreviation => "MLB"},
+	{:sport => "Basketball", :name => "NBA", :abbreviation => "NBA"}
+]
+@leagues.each do |league|
+	League.create(sport_id: Sport.where(title: league[:sport]).first.id, name: league[:name], abbreviation: league[:abbreviation])
+end
